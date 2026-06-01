0282d37cecfbdb5697d9e4a8b8324fb9e019a18f
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-06-01 18:27:35 +0700

    Create App.jsx

---
+++ TIER_5_events_basics/src/App.jsx
@@ -0,0 +1,23 @@
+import ClickEvents from "./components/ClickEvents";
+import InputEvents from "./components/InputEvents";
+import KeyboardEvents from "./components/KeyboardEvents";
+import FormEvents from "./components/FormEvents";
+
+function App() {
+  return (
+    <div>
+      <ClickEvents />
+      <hr />
+
+      <InputEvents />
+      <hr />
+
+      <KeyboardEvents />
+      <hr />
+
+      <FormEvents />
+    </div>
+  );
+}
+
+export default App;
