d075c54317963b22ac07ca64ec1844ffe4edcb00
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-06-01 18:27:55 +0700

    Create App.jsx

---
+++ TIER_6_lists_crud/App.jsx
@@ -0,0 +1,23 @@
+import ListBasics from "./components/ListBasics";
+import CreateItem from "./components/CreateItem";
+import DeleteItem from "./components/DeleteItem";
+import UpdateItem from "./components/UpdateItem";
+
+function App() {
+  return (
+    <div>
+      <ListBasics />
+      <hr />
+
+      <CreateItem />
+      <hr />
+
+      <DeleteItem />
+      <hr />
+
+      <UpdateItem />
+    </div>
+  );
+}
+
+export default App;
