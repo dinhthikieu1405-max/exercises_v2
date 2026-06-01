1d380f35df55dc7741fdfcaddd9e8c30dec1cb41
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-06-01 18:46:29 +0700

    Create 02_variables.js
    [FEATURE] Add variable declaration examples

---
+++ TIER_1_react_flow/02_variables.js
@@ -0,0 +1,37 @@
+// ===== LET =====
+
+let ten = "Minh";
+let tuoi = 20;
+let laSinhVien = true;
+
+console.log("Tên:", ten);
+console.log("Tuổi:", tuoi);
+console.log("Là sinh viên:", laSinhVien);
+
+tuoi = 21;
+
+console.log("Tuổi mới:", tuoi);
+
+// ===== CONST =====
+
+const PI = 3.14;
+const TEN_TRUONG = "Đại học Thủy Lợi";
+
+console.log("PI =", PI);
+console.log("Trường =", TEN_TRUONG);
+
+// ===== VAR =====
+
+var soLanHoc = 1;
+
+console.log("Số lần học:", soLanHoc);
+
+// ===== Thử thách =====
+
+let monHoc = "JavaScript";
+
+const soTinChi = 3;
+
+console.log(
+    `Môn ${monHoc} có ${soTinChi} tín chỉ`
+);
