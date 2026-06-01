078e4c77f3e51e9cc9d4d056bda5dd7a5f1eae3e
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-06-01 18:27:13 +0700

    Create App.jsx

---
+++ TIER_4_useState_basics/App.jsx
@@ -0,0 +1,23 @@
+import NumberState from "./components/NumberState";
+import StringState from "./components/StringState";
+import BooleanState from "./components/BooleanState";
+import MultipleStates from "./components/MultipleStates";
+
+function App() {
+  return (
+    <div>
+      <NumberState />
+      <hr />
+
+      <StringState />
+      <hr />
+
+      <BooleanState />
+      <hr />
+
+      <MultipleStates />
+    </div>
+  );
+}
+
+export default App;
