1b47545cbb49abe0711a1ae79d5c20ba8ff948ed
dinhthikieu1405-max <dinhthikieu1405@gmail.com>
2026-06-01 18:26:55 +0700

    Create Header.jsx

---
+++ TIER_3_component_split/scr/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>Shop Điện Thoại</h1>
+
+      <nav>
+        <a href="/">Trang chủ</a> |
+        <a href="/about"> Giới thiệu</a> |
+        <a href="/contact"> Liên hệ</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
